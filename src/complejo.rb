f434d87fad09dfb17c0f23413bc53b5ff1c613ad
Daniel Luis Martin <alu0100537093@ull.edu.es>
2014-10-16 20:17:01 +0100

    Creados los metodos de suma y resta de la clase Complejo.

--- src/complejo.rb
+++ src/complejo.rb
@@ -13,4 +13,16 @@ attr_reader :r , :im
 	end
 	
 	
+	def suma (r,im) #Metodo que suma numeros complejos
+		re = @r + r
+		ima = @im + im
+		"#{re}+#{ima}i"
+	end
+	
+	def resta (r,im) #Metodo que resta numeros complejos
+		re = @r - r
+		ima = @im - im
+		"#{re}+#{ima}i"
+	end
+	
 end
