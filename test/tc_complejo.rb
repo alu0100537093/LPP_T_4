e6ab3d0270011ea17594863b7e75e4c5207291f6
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2014-10-16 20:20:18 +0100

    Comprobado correcto funcionamiento de la suma y de la resta.

--- test/tc_complejo.rb
+++ test/tc_complejo.rb
@@ -2,11 +2,20 @@ require "../src/complejo.rb"
 require "test/unit"
 
 
-class Test_Fraccion < Test::Unit::TestCase
+class Test_Complejo < Test::Unit::TestCase
 
 
 	
 	def test_simple
 		assert_equal("2+3i", Complejo.new(2,3).to_s)
+	end
+
+	def test_suma
+		assert_equal("7+4i", Complejo.new(4,3).suma(3,1))
+	end
+	
+	
+	def test_resta
+		assert_equal("2+1i", Complejo.new(4,3).resta(2,2))
 	end
 end
